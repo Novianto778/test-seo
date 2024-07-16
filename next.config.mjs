b7bb1ad01747c57c31572b349bba97dab66faa65
novianto778 <63406837+Novianto778@users.noreply.github.com>
2024-07-16 12:37:58 +0700

    header config

--- next.config.mjs
+++ next.config.mjs
@@ -28,4 +28,18 @@ const nextConfig = {
   },
 };
 
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline' https://accounts.google.com https://fonts.googleapis.com https://www.googletagmanager.com;
+    style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+    img-src 'self' blob: data: https://i.ytimg.com https://os-dev.acc.co.id https://os-test.acc.co.id https://one.acc.co.id https://dashboard-career.acc.co.id;
+    font-src 'self' blob: data: https://fonts.gstatic.com;
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-src https://www.youtube.com/;
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`;
+
 export default nextConfig;
